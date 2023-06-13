6eef555c2c57f0596df5d7d9ec192d352aa63569
Zenite50 <136513598+Zenite50@users.noreply.github.com>
2023-06-13 15:30:48 -0600

    Add files via upload
    Incomplete.

---
+++ Suma fila y columna.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main(){
+   
+  int fila=3; 
+  int columna=3;
+  int fila1,fila2,fila3;
+  int columna1,columna2,columna3;  
+  int matriz[fila][columna];	
+
+for(int i=0;i<fila;i++){
+	for(int x=0;x<columna;x++){
+	cout<<"Ingrese los datos de matriz en "<<i<<","<<x<<endl;;  
+	cin>>matriz[i][x];	
+	} 
+	
+
+}      
+for(int i=0;i<fila;i++){
+    for(int x=0;x<columna;x++){
+fila1=matriz[0][1]+matriz[0][2];
+ } 
+} 	
+cout<<"================"<<endl;
+for(int i=0;i<fila;i++){
+	for(int x=0;x<columna;x++){
+	cout<<"Los datos de matriz son: "<<columna1<<endl;
+	}
+}  	
+}
+  	
+
